e3461f5533a4e80e3767aa96b53ef8bb5f60beab
Amtomkraftwerk <antonthielmann@t-online.de>
2020-06-05 14:59:57 +0200

    text preprocessing

--- pkg/scrappy.py
+++ pkg/scrappy.py
@@ -2,7 +2,8 @@ import json
 from time import sleep
 from webbot import Browser
 from bs4 import BeautifulSoup
-
+import pandas as pd
+import re
 
 class SDscraper:
     def __init__(self):
@@ -32,8 +33,30 @@ class SDscraper:
         soup = BeautifulSoup(page, 'html')
         #print(soup.get_text())
         tags = soup.find_all('div', {"class":"text-area-read"})
+        names = soup.find_all('div', {"class":"user-name"})
+        text = []
+        poster = []
         for t in tags:
-            print(t.text)
+            text.append(t.text)
+        for k in names:
+            poster.append(k.text)
+
+        self.data = {"text": text,
+                "poster" : poster}
+
+
+
+    def data_cleaning(self):
+
+        self.data = pd.DataFrame(self.data)
+        self.data = self.data.replace(r'\n',  ' ', regex=True)
+
+        self.data['text'] = [re.sub(' +', ' ', x) for x in self.data['text']]
+        self.data['poster'] = [re.sub(' +', ' ', x) for x in self.data['poster']]
+
+        print(self.data.head(20))
+        print(self.data['poster'].head(4))
+
 
 
 if __name__ == "__main__":
@@ -41,3 +64,4 @@ if __name__ == "__main__":
     sds.login()
     sds.go_to_course()
     sds.scrape()
+    sds.data_cleaning()
