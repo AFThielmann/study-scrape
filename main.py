a84a484b4c700de89a0cbf1703f3d3dd0111a6f9
ybml <ybml@pm.me>
2020-06-05 15:39:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import json
 from pkg import WebScraper
+import pandas as pd
 
 ws = WebScraper()
 with open("login.json", "r") as lg:
     LG = json.load(lg)
 ws.login(lg = LG)
 
-KURSE = ['Mathematik']
+# z.B.
+KURSE = ['Multivariate Time Series Analysis', 
+         'Ökonometrie I/ Econometrics I',
+         'Lineare Modelle',
+         'Informatik',
+         'Differenzial- und Integralrechnung II',
+         'Differential- und Integralrechnung I',
+         'Analytische Geometrie und Lineare Algebra II',
+         'Mathematik']
 
 df_list = []
 for kurs in KURSE:
     ws.go_to_course(course = kurs)
     df = ws.scrape()
-    print(df)
-    df.to_csv('test.csv')
+    df['course'] = kurs
+    df_list.append(df)
+
+df = pd.concat(df_list)
+df.to_csv('df.csv')
